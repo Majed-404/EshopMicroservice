3f62f3c8729f588d79c6af4317da039356f507d9
Mohamed Majed <33943617+Majed-404@users.noreply.github.com>
2024-09-09 15:45:11 +0300

    add create product endpoint

---
+++ src/Services/Catalog/CatalogAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogAPI.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!; //using this expression default! is to Avoiding Nullability Warnings from compiler
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
+    public decimal Price { get; set; }
+}
+
--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -2,8 +2,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+///Mapster, by default, assumes that property names follow PascalCase conventions. 
+///This can cause issues when using lower case property names, 
+///as it may not recognize the properties correctly for mapping. 
+///Here's how you can configure Mapster to work with lowercase property names
+TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.IgnoreCase);
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.MapCarter();
 
 app.Run();
